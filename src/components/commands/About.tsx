5f83dd65ad752cd2bad9da9b82c1e9bfc5b16bdd
sasikaran <81082865+sasi123-sk@users.noreply.github.com>
2024-10-06 15:18:16 +0530

    Update About.tsx

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -17,9 +17,10 @@ const About: React.FC = () => {
       <p>
       Detail-oriented Cyber Security Enthusiast dedicated to safeguarding digital assets against cyber threats. Proficient in identifying vulnerabilities, implementing robust security measures, and conducting thorough assessments. Skilled in utilizing cutting-edge tools to ensure data integrity and confidentiality.<br />
       I am committed to staying updated on emerging security trends and eager to contribute to secure software development in dynamic environments.
-      <br />
+      </p>
+      <p>
       I had the opportunity to lead, securing the 76th position in India in April 2024. This provided me with the chance to guide the team for its advancement, directing our collective efforts toward shared goals.
-      Team Link - https://techn0xploit.vercel.app/
+      <a href="https://techn0xploit.vercel.app/">Team Link</a>
       </p>
     </AboutWrapper>
   );
