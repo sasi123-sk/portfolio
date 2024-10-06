fe38c1db4009eb5ea4c22258c98d17c8f299d9f9
sasikaran <81082865+sasi123-sk@users.noreply.github.com>
2024-10-06 15:12:36 +0530

    Update About.tsx

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -12,11 +12,13 @@ const About: React.FC = () => {
       </p>
       <p>
         I'm <HighlightAlt>a Cyber Security Enthusiast</HighlightAlt> based in Chennai,
-        TamilNadu, India.
+        Tamil Nadu, India.
       </p>
       <p>
       Detail-oriented Cyber Security Enthusiast dedicated to safeguarding digital assets against cyber threats. Proficient in identifying vulnerabilities, implementing robust security measures, and conducting thorough assessments. Skilled in utilizing cutting-edge tools to ensure data integrity and confidentiality.<br />
-      Committed to staying updated on emerging security trends and eager to contribute to secure software development in dynamic environments.
+      I am committed to staying updated on emerging security trends and eager to contribute to secure software development in dynamic environments.
+
+      I had the opportunity to lead, securing the 76th position in India in April 2024. This provided me with the chance to guide the team for its advancement, directing our collective efforts toward shared goals.
       </p>
     </AboutWrapper>
   );
